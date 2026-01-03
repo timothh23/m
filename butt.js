8a6ffeedee869d76520c5d8ae69362bb1544f975
timothh23 <timothh_23@yahoo.com>
2026-01-03 17:20:07 -0500

    e
    e

---
+++ butt.js
@@ -0,0 +1,22 @@
+class Booty
+{
+  getInfo()
+  { return
+     {
+        id: 'booty'
+        name: 'Booty Code'
+       blocks:
+       [
+         opcode: 'stinknumber'
+       blockType: Scratch.BlockType.REPORTER,
+         text: 'Stink number'
+       ]
+     };
+    
+  }
+  stinknumber()
+  {
+    return no;
+  }
+}
+Scratch.extensions.register(new Booty());
